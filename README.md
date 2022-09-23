82d1b7ad63fbf0378ccf6a165c890615e3be3f8c
Arth Tyagi <arthtyagi7@gmail.com>
2022-09-23 06:06:02 +0530

    well it works... BLAZINGLY FAST

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # serverless-userpics
+
 Instantly get user avatars via an API call. Configurable af.
+I was using avatars.dicebear.com for a while, but I found some better avatars and decided to host my own serverless version of it.
+
+Will be using this in production on [DevClad](https://devclad.com).
+
+## Features
+
+- [x] Generate avatars on the fly
+- [x] Tiny as fuck even with stupid inline comments (~ 100 lines of code)
+- [x] Serverless on the *EDGEEEEEEEEE*. zooom bitch
+- [x] Configurable as fuck
+
+## Config
+
+``` go
+accountId := os.Getenv("ACCOUNT_ID")
+bucketName := os.Getenv("BUCKET_NAME")
+accessKeyId := os.Getenv("API_ACCESS_KEY")
+accessKeySecret := os.Getenv("API_SECRET_KEY")
+```
+
+For environment variables, I’m using [godotenv](https://github.com/joho/godotenv) to load them from a .env file.
+
+### Services
+
+| Resource | Service |
+| --- | --- |
+| Storage | Cloudflare R2 (S3 compatible) |
+| Serverless Hosting | Vercel (AWS Lambda) |
+| Avatars? | [Craftwork.design](https://craftwork.design) |
+
+---
+
+### Todo
+
+- [ ] Add more avatars
+- [ ] Host lol
+- [ ] Make the function URL public (?)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+
+	// external
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/joho/godotenv"
+)
+
+type S3Response struct {
+	Contents []struct {
+		Key string `json:"Key"`
+	} `json:"Contents"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil { log.Fatal("Error loading .env file") }
+
+	// ENV INITIALIZATION
+	accountId := os.Getenv("ACCOUNT_ID")
+	bucketName := os.Getenv("BUCKET_NAME")
+	accessKeyId := os.Getenv("API_ACCESS_KEY")
+	accessKeySecret := os.Getenv("API_SECRET_KEY")
+
+	// === initialization shit ===
+
+	// AWS CONFIGURATION (using V2 SDK - https://developers.cloudflare.com/r2/examples/aws-sdk-go/ gives a neat example btw)
+	r2Resolver := aws.EndpointResolverWithOptionsFunc(
+		func(service, region string, options ...interface{},
+			) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
+		}, nil
+	})
+
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+	config.WithEndpointResolverWithOptions(r2Resolver),
+	config.WithCredentialsProvider(
+		credentials.NewStaticCredentialsProvider(
+			accessKeyId, accessKeySecret, "",
+			),
+		),
+	)
+	if err != nil {log.Fatal(err)}
+
+	client := s3.NewFromConfig(cfg)
+
+	// test out
+	// fmt.Println(ImageList(client, bucketName, accountId))
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ImageList(client, bucketName, accountId)
+	}()
+
+	wg.Wait()
+
+}
+
+func ImageList(client *s3.Client, bucketName string, accountId string) string {
+
+	// LIST OF IMAGES
+	// (https://developers.cloudflare.com/r2/data-access/s3-api/api/#implemented-object-level-operations/) has list of available operations
+
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	}
+
+	result, err := client.ListObjectsV2(context.TODO(), input)
+	if err != nil {log.Fatal(err)}
+
+	// RESPONSE HANDLING
+
+	var s3Response S3Response
+	jsonString, err := json.Marshal(result)
+	if err != nil {log.Fatal(err)}
+	json.Unmarshal(jsonString, &s3Response)
+
+	// i have to use time as a source?! lol python is better hAhAhA (ofc not wtf)
+	goddamnSource := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(goddamnSource)
+	randomNumber := r.Intn(len(s3Response.Contents))
+	randomImage := s3Response.Contents[randomNumber].Key
+
+	generatedURl := fmt.Sprintf("https://%s.r2.cloudflarestorage.com/%s", accountId, randomImage)
+	return generatedURl
+}
